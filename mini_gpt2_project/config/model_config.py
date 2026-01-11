c243e6b17891400c938f88099a6ff785c24f43ff
Nilesh Charan <nileshcharan.s@gmail.com>
2026-01-11 14:14:13 +0530

    Bug fixes

--- mini_gpt2_project/config/model_config.py
+++ mini_gpt2_project/config/model_config.py
@@ -44,7 +44,7 @@ class ModelConfig:
     api_provider: str = "groq" # Options: "gemini", "groq", "huggingface"
     
     # Using Llama 3 70B (Fast & High Intelligence)
-    api_model_name: str = "llama3-70b-8192"     
+    api_model_name: str = "llama-3.3-70b-versatile"     
     
     # --- HARDCODED KEYS ---
     gemini_key: str = "AIzaSyCRZKG8wDenGmHSLJPL21ZMIRBW23hOlgk"
